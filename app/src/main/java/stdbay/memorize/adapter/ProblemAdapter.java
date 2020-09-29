90577b75dee0cc5a4437c269d1712bad30a98ffb
stdbay <1224342775@qq.com>
2020-09-30 00:58:58 +0800

    再次合并

--- app/src/main/java/stdbay/memorize/adapter/ProblemAdapter.java
+++ app/src/main/java/stdbay/memorize/adapter/ProblemAdapter.java
@@ -1,9 +1,15 @@
 package stdbay.memorize.adapter;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
+=======
+import android.annotation.SuppressLint;
+
+import androidx.annotation.Nullable;
+>>>>>>> f71716a... 完成相册功能
 =======
 import android.annotation.SuppressLint;
 
@@ -26,12 +32,16 @@ public class ProblemAdapter extends BaseQuickAdapter<ProblemItem, BaseViewHolder
 
 
     public ProblemAdapter(int layoutResId, @Nullable List<ProblemItem> data) {
+<<<<<<< HEAD
+>>>>>>> f71716a... 完成相册功能
+=======
 >>>>>>> f71716a... 完成相册功能
         super(layoutResId, data);
     }
 
     @SuppressLint("ResourceType")
     @Override
+<<<<<<< HEAD
 <<<<<<< HEAD
     protected void convert(BaseViewHolder helper, Object item) {
 
@@ -41,6 +51,13 @@ public class ProblemAdapter extends BaseQuickAdapter<ProblemItem, BaseViewHolder
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
     }
+=======
+    protected void convert(BaseViewHolder helper, ProblemItem item) {
+        helper.setText(R.id.name, "题目"+item.getId())
+                .setText(R.id.description, "描述"+item.getCreateTime())
+                .setBackgroundRes(R.id.list_item, R.drawable.gray_corner);
+    }
+>>>>>>> f71716a... 完成相册功能
 =======
     protected void convert(BaseViewHolder helper, ProblemItem item) {
         helper.setText(R.id.name, "题目"+item.getId())
